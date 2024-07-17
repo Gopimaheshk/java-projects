112f466e1661eb52edc9b3865f34089337455135
Gopi Mahesh Kamidi <155874435+Gopimaheshk@users.noreply.github.com>
2024-07-17 13:01:05 +0530

    updated task1

--- task 1.java
+++ task 1.java
@@ -10,3 +10,16 @@ float f=(c*9/5)+32;
 System.out.println("The temperature in Fahrenheit is :"+f);
 }
 }
+
+/*convert celcius to fahrenheit*/
+import java.util.Scanner;
+public class FahrenheitToC
+{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the temperature in Fahrenheit :");
+float f=s.nextFloat();
+float c=(c*9/5)+32;
+System.out.println("The temperature in Celsius is :"+c);
+}
+}
